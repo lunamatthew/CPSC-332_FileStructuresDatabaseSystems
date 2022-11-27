b7a55fd44deb7e7a48a835f7f86b6cb1f0806fb7
Matthew Luna <57422802+lunamatthew@users.noreply.github.com>
2022-11-27 06:01:24 -0800

    Add files via upload

---
+++ QueriesFSDBTermProject.sql
@@ -0,0 +1,87 @@
+-- ==================================================================================
+
+-- ==================== ENROLLMENT RECORDS ==========================================
+-- Section ID was not consistent with increment, (Possibly caused when updating a record)
+-- so section_id may be different when transferring
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (1, 012345678, "B-");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (2, 012345678, "A+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (6, 012345678, "C+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (5, 012345678, "B");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (6, 012345677, "D");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (7, 012345677, "C+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (8, 012345677, "B");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (1, 012345677, "A");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (2, 012345677, "A+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (5, 012345676, "A+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (7, 012345676, "B-");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (8, 012345675, "C-");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (4, 012345674, "D+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (6, 012345674, "A");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (1, 012345673, "B");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (5, 012345673, "A");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (7, 012345673, "A+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (2, 012345672, "C+");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (8, 012345672, "B");
+
+
+INSERT INTO enrollment(section_id, student_id, grade)
+VALUES (4, 012345671, "A+");
+
+
+-- ==================================================================================
